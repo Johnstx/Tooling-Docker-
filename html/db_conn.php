c35335027a169c6d23d91d96bbf2088d870753e0
Johnstx <johnukiwe@gmail.com>
2024-09-02 23:37:47 +0100

    updates

--- html/db_conn.php
+++ html/db_conn.php
@@ -19,6 +19,7 @@ $dbname = $_ENV["MYSQL_DBNAME"]; // input dbname
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
